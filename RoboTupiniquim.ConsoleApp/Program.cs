37577503796c9dd8632f5b6eb61a741f1c87d5ac
Alec Luí <dr.elucidator@gmail.com>
2026-03-25 15:50:18 -0300

    Adicionadas classes estáticas

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,8 +1,77 @@
-﻿namespace RoboTupiniquim.ConsoleApp;
+﻿﻿namespace RoboTupiniquim.ConsoleApp;
 
 using System;
 
-class program
+static class Robot
+{
+    public static int RobotX = 2;
+    public static int RobotY = 1;
+    public static char Orientation = 'N';
+
+    public static void Config(int PositionX, int PositionY, char Orient)
+    {
+        RobotX = PositionX;
+        RobotY = PositionY;
+        Orientation = Orient;
+    }
+
+    public static void RunCommand(char[,] Grid, string Command)
+    {
+        for (int i = 0; i < Command.Length; i++)
+        {
+            Console.WriteLine("Pressione ENTER para avançar...");
+            Console.ReadLine();
+            char c = Command[i];
+
+            if (c == 'E')
+            {
+                Robot.TurnLeft();
+            }
+            else if (c == 'D')
+            {
+                Robot.TurnRight();
+            }
+            else if (c == 'M')
+            {
+                Grid[RobotX, RobotY] = '.';
+                Robot.Move();
+                Grid[RobotX, RobotY] = 'R';
+            }
+
+            Program.ShowGrid(Grid, RobotX, RobotY, Orientation);
+        }
+    }
+
+    public static void Move()
+    {
+        if (Orientation == 'N')
+            RobotX++;
+        else if (Orientation == 'S')
+            RobotX--;
+        else if (Orientation == 'L')
+            RobotY++;
+        else if (Orientation == 'O')
+            RobotY--;
+    }
+
+    public static void TurnLeft()
+    {
+        if (Orientation == 'N') Orientation = 'O';
+        else if (Orientation == 'O') Orientation = 'S';
+        else if (Orientation == 'S') Orientation = 'L';
+        else Orientation = 'N';
+    }
+
+    public static void TurnRight()
+    {
+        if (Orientation == 'N') Orientation = 'L';
+        else if (Orientation == 'L') Orientation = 'S';
+        else if (Orientation == 'S') Orientation = 'O';
+        else Orientation = 'N';
+    }
+}
+
+class Program
 {
     static void Main(string[] arg)
     {
@@ -15,56 +84,21 @@ class program
             for (int z = 0; z < GridColumns; z++)
                 Grid[i, z] = '.';
         }
-        int RobotX = 2;
-        int RobotY = 1;
-        char Orientation = 'N';
-        Grid[RobotX, RobotY] = 'R';
+
+        Robot.Config(2, 1, 'N');
+        Grid[Robot.RobotX, Robot.RobotY] = 'R';
 
         string Command = "EMEMEMEMM";
 
-        ShowGrid(Grid, RobotX, RobotY, Orientation);
-        Console.WriteLine($"Posição inicial do Robô Tupiniquim: {RobotY} {RobotX} {Orientation}");
+        ShowGrid(Grid, Robot.RobotX, Robot.RobotY, Robot.Orientation);
+        Console.WriteLine($"Posição inicial do Robô Tupiniquim: {Robot.RobotY} {Robot.RobotX} {Robot.Orientation}");
 
-        for (int i = 0; i < Command.Length; i++)
-        {
-            Console.WriteLine("Pressione ENTER para avançar...");
-            Console.ReadLine();
-            char c = Command[i];
+        Robot.RunCommand(Grid, Command);
 
-            if (c == 'E')
-            {
-                if (Orientation == 'N') Orientation = 'O';
-                else if (Orientation == 'O') Orientation = 'S';
-                else if (Orientation == 'S') Orientation = 'L';
-                else Orientation = 'N';
-            }
-            else if (c == 'D')
-            {
-                if (Orientation == 'N') Orientation = 'L';
-                else if (Orientation == 'L') Orientation = 'S';
-                else if (Orientation == 'S') Orientation = 'O';
-                else Orientation = 'N';
-            }
-            else if (c == 'M')
-            {
-                Grid[RobotX, RobotY] = '.';
-
-                if (Orientation == 'N')
-                    RobotX++;
-                else if (Orientation == 'S')
-                    RobotX--;
-                else if (Orientation == 'L')
-                    RobotY++;
-                else if (Orientation == 'O')
-                    RobotY--;
-
-                Grid[RobotX, RobotY] = 'R';
-            }
-            ShowGrid(Grid, RobotX, RobotY, Orientation);
-        }
-        Console.WriteLine($"Posição final do Robô Tupiniquim: {RobotY} {RobotX} {Orientation}");
+        Console.WriteLine($"Posição final do Robô Tupiniquim: {Robot.RobotY} {Robot.RobotX} {Robot.Orientation}");
     }
-    static void ShowGrid(char[,] Grid, int RobotX, int RobotY, char Orientation)
+
+    public static void ShowGrid(char[,] Grid, int RobotX, int RobotY, char Orientation)
     {
         Console.Clear();
         for (int i = Grid.GetLength(0) - 1; i >= 0; i--)
